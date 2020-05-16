01a6917b1eba104005de02ca0a61847ff02a5f2e
Dim <me@dimps.name>
2020-05-15 20:10:44 -0400

    Listing only

--- index.js
+++ index.js
@@ -32,12 +32,12 @@ Ftp.ls(".", (err, res) => {
   res.forEach(file => console.log(file.name));
 });
 
-Ftp.get("public_html/constructorio/constructorio_feed.txt", "files/constructorio_feed.txt", err => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log("File copied successfully!");
-});
+// Ftp.get("public_html/constructorio/constructorio_feed.txt", "files/constructorio_feed.txt", err => {
+//   if (err) {
+//     return console.error(err.message);
+//   }
+//   console.log("File copied successfully!");
+// });
 
 // Ftp.raw("quit", (err, data) => {
 //   if (err) {
