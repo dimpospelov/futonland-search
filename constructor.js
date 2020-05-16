201338df916b7ed35d2ce9948094c2cc30d3ea66
Dim <me@dimps.name>
2020-05-16 01:27:04 -0400

    Empty file

---
+++ constructor.js
@@ -0,0 +1,16 @@
+var ConstructorIO = require('constructorio');
+
+
+var constructorio = new ConstructorIO({
+	apiToken: "jO0lkOIKXAkWQniCb0Bz", 
+	apiKey: "anbtAGy3ebXPRjpKaP8b"
+});
+
+
+constructorio.verify(function(error, response) {
+  if (error) {
+    console.log("Error: ", error);
+  } else {
+    console.log("Response: ", response);
+  }
+});
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var fs = require ('fs'),
 	Ftp = require('ftp'),
-	ConstructorIO = require('constructorio');
+	constructor = require('./constructor.js');
 
 
 var ftp_user = "constructorio";
 var ftp_pass = "XFSrd0UFZSzG";
+
 
 fs.mkdir('tmp', (err) => {
 	if (err) throw err;
@@ -22,7 +23,8 @@ ftp.on('ready', function() {
 		for (var i=0; i<files.length; i++) {
 			console.log(files[i]);
 		}
-
+		
+		constructor();
 		ftp.end();
 
       });
@@ -37,50 +39,3 @@ ftp.connect({
 	user: ftp_user,
 	password: ftp_pass
 });
-
-
-
-// var constructorio = new ConstructorIO({
-// 	apiToken: "jO0lkOIKXAkWQniCb0Bz", 
-// 	apiKey: "anbtAGy3ebXPRjpKaP8b"
-// });
-
-
-// constructorio.verify(function(error, response) {
-//   if (error) {
-//     console.log("Error: ", error);
-//   } else {
-//     console.log("Response: ", response);
-//   }
-// });
-
-
-
-// const Ftp = new jsftp({
-//   host: "feeds.futonland.com",
-//   port: 21,
-//   user: ftp_user,
-//   pass: ftp_pass
-// });
-
-// Ftp.auth(ftp_user, ftp_pass, (err, res) => {
-// 	if (err) return console.error(err.message);
-// });
-
-// Ftp.ls("public_html", (err, res) => {
-//   res.forEach(file => console.log(file.name));
-// });
-
-// Ftp.get("public_html/constructorio_feed2.txt", "files/constructorio_feed.txt", err => {
-//   if (err) return console.error(err.message);
-//   console.log("File copied successfully!");
-// });
-
-// Ftp.raw("quit", (err, data) => {
-//   if (err) {
-//     return console.error(err);
-//   }
- 
-//   console.log("Bye!");
-// });
-
